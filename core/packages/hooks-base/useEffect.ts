20a950ecbb91f887557eee673f4ad032e0420769
kabudaaixuexi <kaburda@163.com>
2022-09-13 14:38:59 +0800

    👏 build: build edition

--- core/packages/hooks-base/useEffect.ts
+++ core/packages/hooks-base/useEffect.ts
@@ -1,1 +1,22 @@
-// todo
+let activeEffect = undefined;
+class ReactiveEffect {
+    active = true;
+    deps = [];
+    constructor(fn: Target) {
+        (this as Target).fn = fn
+    }
+    run() {
+        if(!this.active) {(this as Target).fn()};
+        try {
+            activeEffect = this;
+            return (this as Target).fn()
+        } finally {
+            activeEffect = undefined;
+        }
+    }
+}
+
+export default (fn: Target) => {
+    const _effect = new ReactiveEffect(fn);
+    _effect.run()
+}
